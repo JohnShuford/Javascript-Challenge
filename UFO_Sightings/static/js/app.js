5f7d13e36ac981cdb6c64a00000eeef447802a1f
Johnny Shuford <johnnyshuford@Johnnys-MacBook-Pro.local>
2020-12-19 22:15:10 -0700

    running the filter

--- UFO_Sightings/static/js/app.js
+++ UFO_Sightings/static/js/app.js
@@ -1,19 +1,49 @@
-// Get a references
-// table body
+//create the table on the page with the ufo data
+// rename the data to something more specific
+var ufoData = data;
+
+// table body refrence
 var tbody = d3.select("#ufo-table");
-// button
+
+// render the table
+// ufoData.forEach((sightings) => {
+//   var row = tbody.append("tr");
+//   Object.entries(sightings).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+//filer through the table for a specific date
+// button refrence
 var button = d3.select("#filter-btn");
-console.log(button);
-// form
-var form = d3.select("#")
 
-// from data.js
-var tableData = data;
+// form refrence
+var form = d3.select(".form-group");
 
-tableData.forEach((weatherReport) => {
+// Create event handlers 
+button.on("click", dateSearch);
+form.on("submit",dateSearch);
+
+function dateSearch(){
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  var inputValue = d3.select('#datetime').property('value');
+
+  console.log(inputValue);
+
+  var filteredDate = ufoData.filter(item => item.datetime===inputValue);
+
+  console.log(filteredDate);
+
+  filteredDate.forEach((sightings) => {
     var row = tbody.append("tr");
-    Object.entries(weatherReport).forEach(([key, value]) => {
+    Object.entries(sightings).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+}
+
