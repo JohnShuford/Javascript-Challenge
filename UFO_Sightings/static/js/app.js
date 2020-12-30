2a9b7a196468e1b55a0c26942047a855cb7ede67
Johnny Shuford <johnnyshuford@Johnnys-MacBook-Pro.local>
2020-12-30 13:32:48 -0700

    getting the necessary requirements debugged

--- UFO_Sightings/static/js/app.js
+++ UFO_Sightings/static/js/app.js
@@ -37,24 +37,14 @@ function dateSearch(){
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // clearing();
-
   var inputValue = d3.select('#datetime').property('value');
 
   console.log(inputValue);
 
-  var filteredDate = ufoData.filter(item => item[key]===inputValue);
+  var filteredDate = ufoData.filter(item => item.datetime === inputValue);
 
   console.log(filteredDate);
 
   buildtable(filteredDate);
-
-  // filteredDate.forEach((sightings) => {
-  //   var row = tbody.append("tr");
-  //   Object.entries(sightings).forEach(([key, value]) => {
-  //     var cell = row.append("td");
-  //     cell.text(value);
-  //   });
-  // });
 }
 
