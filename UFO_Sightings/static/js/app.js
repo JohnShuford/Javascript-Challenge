bdc13ee351af9c4eefa23ada4be002db59dcc41b
Johnny Shuford <johnnyshuford@Johnnys-MacBook-Pro.local>
2020-12-30 13:35:36 -0700

    adding ya comments

--- UFO_Sightings/static/js/app.js
+++ UFO_Sightings/static/js/app.js
@@ -21,8 +21,8 @@ function buildtable(data){
 // render the table
 buildtable(ufoData);
 
-//filer through the table for a specific date
-// button refrence
+/*filer through the table for a specific date
+ button refrence */
 var button = d3.select("#filter-btn");
 
 // form refrence
@@ -37,14 +37,19 @@ function dateSearch(){
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
+  //getting the searched for date from the input field
   var inputValue = d3.select('#datetime').property('value');
 
+  // logging the value
   console.log(inputValue);
 
+  // filtering the data by the specific date
   var filteredDate = ufoData.filter(item => item.datetime === inputValue);
 
+  //loggint the data
   console.log(filteredDate);
 
+  // building the filtered table
   buildtable(filteredDate);
 }
 
