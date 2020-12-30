0842b3cb8f4e67969fc30007016d0a050250736b
Johnny Shuford <johnnyshuford@Johnnys-MacBook-Pro.local>
2020-12-30 14:39:44 -0700

    getting the bonus

--- UFO_Sightings_Bonus/static/js/app_bonus.js
+++ UFO_Sightings_Bonus/static/js/app_bonus.js
@@ -13,17 +13,18 @@ function buildtable(data){
   data.forEach((sightings) => {
     var row = tbody.append("tr");
     Object.entries(sightings).forEach(([key, value]) => {
-      data.filter(item => item[key]===inputValue);
       var cell = row.append("td");
       cell.text(value);
       });
   });
 };
 
+function filterData(data){
+  data.filter(item => item[key]===inputValue);
+}
+
 //render the table upon loading
 buildtable(ufoData);
-
-var filterData = [];
 
 //filer through the table for a specific date
 // button refrence
@@ -32,29 +33,52 @@ var button = d3.select("#filter-btn");
 // form refrence
 var form = d3.select(".form-group");
 
-// filter references
+// filter inputs and create its own event handler
 var filter = d3.selectAll(".filter").on("change",captureFilterData)
 
 // Create event handlers 
-button.on("click", dateSearch);
-form.on("submit",dateSearch);
-
-
+button.on("click", captureFilterData);
+form.on("submit", captureFilterData);
 
 function captureFilterData(){
-    var element = d3.select(this).select("input");
-    var inputValue  = element.property('value');
-    var id = element.attr('id');
-    if(inputValue){
-        filterData[id] = inputValue;
-    }
-    else{
-        delete filterData[id];
-    }
+  //creating an empty array for the filtered data
+  var filter = [];
 
-    buildFilterTable();
+  // selecting the inputed element using this
+  var element = d3.select(this).select("input");
 
-}
+  // finding the value from the element
+  var inputValue  = element.property('value');
+
+  // loggint the filter
+  console.log(inputValue);
+
+  // finding the id from the element
+  var id = element.attr('id');
+
+  // loggin the id
+  console.log(id);
+
+  // an if statement for creating the filter array
+  if(inputValue){
+      filter[id] = inputValue;
+  }
+  else {
+      delete filter[id];
+  }
+
+  // logging the current filters
+  console.log(filter)
+
+  // filtering the data
+  filteredData = ufoData.filter(item => item[id]===inputValue);
+
+  // logging the filtered data
+  console.log(filteredData);
+
+  // building the table with the filtered data
+  buildtable(filteredData);
+};
 
 
 
